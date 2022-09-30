91398e0f70005acb4b537fb674d1d83dfcad424e
patriicke <patrickndayambaje4@gmail.com>
2022-09-30 23:48:31 +0200

    This is the test folder for code testing

---
+++ TEST/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // double a = 1;
+    // cout << a;
+    int a = 5;
+    int b = 12;
+    int c = a | b;
+    cout << c << endl;
+    return 1;
+}
