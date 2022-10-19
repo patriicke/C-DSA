17759f8042afb0e6339485869242d81021e50762
patriicke <patrickndayambaje4@gmail.com>
2022-10-19 08:12:23 +0200

    I am done with the square root checking

---
+++ HOMEWORK/Q3/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+bool isNumberInteger(double N);
+
+int main(){
+    int i = 1, j = 1;
+    for ( i ; i < 100; i++){
+        for ( j = i + 1; j < 100; j++){
+            if(isNumberInteger(sqrt(pow(i,2)+pow(j,2)))){
+            cout << "( " << i << "," << j << " )" << endl;
+            }
+        }
+    }
+    return 0;
+}
+
+bool isNumberInteger(double DOUBLE_NUM){
+    int INT_NUM = DOUBLE_NUM;
+    double CHECKER = DOUBLE_NUM - INT_NUM;
+    if(CHECKER > 0){
+      return false;
+    }else{
+      return true;
+    }
+}
+
