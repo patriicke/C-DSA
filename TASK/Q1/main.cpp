e113c9f324779a33afbedb0abbe80858a67c7514
patriicke <patrickndayambaje4@gmail.com>
2022-10-19 07:40:10 +0200

    This is trying to resolve the task

---
+++ TASK/Q1/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int  sum =0 , number;
+    cout << "Enter number you would like to enter: ";
+    cin >> number;
+    if(number > 0) {
+    for (int i = 0; i < number; i++){
+        
+    }
+    }else {
+        cout << "Thank you for using the program!" << endl;
+    }
+    return 0;
+    
+}
