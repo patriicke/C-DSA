c917a9b52a7d2ca93f419783ad7476eed1e7c3e5
patriicke <patrickndayambaje4@gmail.com>
2022-10-24 18:03:42 +0200

    I have made a palindrome in 4 minutes

--- SECOND HOMEWORK/Q20/main.cpp
+++ SECOND HOMEWORK/Q20/main.cpp
@@ -1,19 +1,24 @@
-// #include<iostream>
-// using namespace std;
+#include<iostream>
 
-// string reverseString (string str){
-//     string temp = "";
-//     for (int i = 0; i < str.length(); i++)
-//     {
-//         temp.append(str[]);
-//     }
+using namespace std;
+
+bool palindromeChecker (string str1){
+    int strlen = str1.length();
+    bool checker = true;
+    for (size_t i = 0; i < strlen; i++){
+        if (str1[i] != str1[strlen - (i + 1) ]){
+            checker = false;
+        }
+    }
+    return checker;
+}
     
-// return "da";
-// }
-
-// int main (){
-//     string name = "patrick";
-//     cout << name.length() << endl;
-//     cout << name.reserve() << endl;
-//     return 0;
-// }
+int main (){
+    string text;
+    cout << "Palindrome checker. Enter a string: ";
+    cin >> text;
+    if(palindromeChecker)cout << text << " is a palindrome";
+    else cout << text << " is not a palindrome";
+    cout << endl;
+    return 0;
+}
