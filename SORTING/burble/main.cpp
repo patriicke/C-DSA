bcf81fdd686ea11b946a60089f901d9fe9050783
patriicke <patrickndayambaje4@gmail.com>
2022-11-11 10:00:54 +0200

    [burble sort]: This is withhout break cases

--- SORTING/burble/main.cpp
+++ SORTING/burble/main.cpp
@@ -4,16 +4,23 @@ using namespace std;
 
 int main (){
     int i,j,n,temp;
-    int numbers[] = {12,55,5,18};
+    int numbers[] = {66,78,21,10,2,44};
     n = sizeof(numbers)/sizeof(numbers[0]);
-    for (int i = 0; i < n - 1; i++){
+    for (int i = 1; i < n ; i++){
+        cout << (i) << " iteration: "<< endl;
         for (int j = 0; j < n - 1; j++){
             if (numbers[j] > numbers[j + 1]){
                 temp = numbers[j];
                 numbers[j] = numbers[j + 1];
                 numbers[j + 1] = temp;
             }
-            
+            cout << endl;
+            cout << "j="<< j<< endl;
+            for (int p = 0; p < n; p++)
+            {
+                cout << numbers[p] << "\t";
+            }
+            cout << endl;
         }
     }
     cout << "[";
