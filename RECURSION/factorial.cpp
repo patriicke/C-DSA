a2a61a8f707a606e664ee6c55866489dd26e0eaa
patriicke <patrickndayambaje4@gmail.com>
2022-11-18 20:21:09 +0200

    [feat]: This is dsa recursion

---
+++ RECURSION/factorial.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int factorial(int number){
+    if(number == 0){
+        return 1;
+    }
+    return number * factorial(number - 1);
+}
+
+int main (){
+    
+    cout << factorial(5) << endl;
+
+    return 0;
+}
--- SECOND HOMEWORK/Q22/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-
-    }else if (number % 7 == 0){
